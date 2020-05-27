32c7320152f1f03f1c04c4e805e6610db95572eb
Guilherme <guiazetri@uninove.edu.br>
2020-05-26 22:48:15 -0300

    TEste 4

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -10,6 +10,8 @@ namespace TesteGit
             Console.WriteLine("Bom dia!");
 
             Console.WriteLine("TESTE");
+            Console.WriteLine("TESTE2");
+             Console.WriteLine("TESTE3");
         }
     }
 }
